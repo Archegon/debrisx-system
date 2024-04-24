bc4b86157b70d1e7bdaea93867262cd413bf0d3f
Archegon <ng.ta.wei@outlook.com>
2024-04-24 15:16:06 +0800

    Tensorflow object detection

--- modules/camera/camera_handler.py
+++ modules/camera/camera_handler.py
@@ -2,6 +2,33 @@ from picamera2 import Picamera2
 import time
 import io
 
+def take_still_image():
+    camera = Picamera2()
+    
+    # Use a suitable configuration for still images or low-resolution streaming
+    camera_config = camera.create_still_configuration()
+    camera.configure(camera_config)
+    camera.start()
+
+    try:
+        # Allow camera some time to adjust to conditions
+        time.sleep(2)
+        stream = io.BytesIO()
+        
+        # Request a JPEG capture
+        camera.capture_file(stream, format='jpeg')
+        stream.seek(0)
+        frame = stream.read()
+
+        # Reset stream for next capture
+        stream.seek(0)
+        stream.truncate()
+        
+        return frame
+
+    finally:
+        camera.stop()
+
 def generate_frames():
     camera = Picamera2()
     
