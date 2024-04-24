57504299d777a2cd9432aec9b8ebb48e9a8ebd39
Archegon <ng.ta.wei@outlook.com>
2024-04-24 11:41:42 +0800

    Camera stream

--- server/app.py
+++ server/app.py
@@ -1,6 +1,9 @@
-from picamera2 import Picamera2
+from flask import Flask, Response
+from picamera2 import Picamera2, Preview
 import time
 import io
+
+app = Flask(__name__)
 
 def generate_frames():
     camera = Picamera2()
@@ -25,9 +28,22 @@ def generate_frames():
             # Reset stream for next capture
             stream.seek(0)
             stream.truncate()
-            
-            yield frame
+
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
     finally:
         camera.stop()
 
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/')
+def index():
+    # Return a simple webpage with the video stream embedded
+    return Response('<html><body><img src="/video_feed"></body></html>')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
